4ae1f8f8f7a9f4184183e975ebfc6d96d31aeb72
doctori <lerouge.pierre@gmail.com>
2020-11-04 16:23:40 +0100

    removing app moving to another project

--- app/scripts/translations.js
+++
@@ -1,5 +0,0 @@
-angular.module('openBicycleDatabaseApp').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
-    gettextCatalog.setStrings('fr_FR', {"About":"A propos","Brand":"Marque","Component Name":"Nom Du Composant","Components :":"Composants :","Contact":"Contact","Home":"Maison","Type":"Type","Year":"Année de Production"});
-/* jshint +W100 */
-}]);
